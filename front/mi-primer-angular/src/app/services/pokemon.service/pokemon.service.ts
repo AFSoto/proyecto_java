cc62c125a12abcc01247a8470e195fd015f27862
Andres Felipe Soto Quintero <andresfelipesotoquintero@gmail.com>
2026-02-03 04:13:28 +0100

    dia 11 (bases de datos)

--- front/mi-primer-angular/src/app/services/pokemon.service/pokemon.service.ts
+++ front/mi-primer-angular/src/app/services/pokemon.service/pokemon.service.ts
@@ -1,6 +1,17 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
+export interface PokemonResponse{
+  count:number;
+  next:string | null;
+  previous:string;
+  result: PokemonInterface[]
+}
+
+export interface PokemonInterface{
+  name: string;
+  url: string;
+}
 @Injectable({
   providedIn: 'root',
 })
@@ -8,7 +19,7 @@ export class PokemonService {
   constructor(private http: HttpClient){}
 
   getPokemons(){
-    return this.http.get<any[]>('https://pokeapi.co/api/v2/pokemon?limit=8')
+    return this.http.get<PokemonResponse>('https://pokeapi.co/api/v2/pokemon?limit=8')
   }
 
 }
--- front/mi-primer-angular/src/app/view/pokemon/pokemon.component/pokemon.component.ts
+++ front/mi-primer-angular/src/app/view/pokemon/pokemon.component/pokemon.component.ts
@@ -1,6 +1,6 @@
 import { ChangeDetectorRef, Component } from '@angular/core';
 import { PokemonService } from '../../../services/pokemon.service/pokemon.service';
-
+import { PokemonInterface } from '../../../services/pokemon.service/pokemon.service';
 @Component({
   selector: 'app-pokemon.component',
   imports: [],
@@ -8,11 +8,11 @@ import { PokemonService } from '../../../services/pokemon.service/pokemon.servic
   styleUrl: './pokemon.component.css',
 })
 export class PokemonComponent {
-  pokemons:any[] = [];
+  pokemons:PokemonInterface[] = [];
 
   constructor(private pokemonService:PokemonService,private chp: ChangeDetectorRef){
-    this.pokemonService.getPokemons().subscribe((data:any) =>{
-      this.pokemons = data.results;
+    this.pokemonService.getPokemons().subscribe(data =>{
+      this.pokemons = data;
       console.log(data);
       this.chp.detectChanges();
     })
