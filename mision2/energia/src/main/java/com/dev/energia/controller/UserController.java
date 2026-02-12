7caa76e2026bc219f2e544e0726bdb32bd9db911
Andres Felipe Soto Quintero <andresfelipesotoquintero@gmail.com>
2026-02-11 22:08:31 -0500

    metodos get , post , put with postman

--- mision2/energia/src/main/java/com/dev/energia/controller/UserController.java
+++ mision2/energia/src/main/java/com/dev/energia/controller/UserController.java
@@ -6,23 +6,48 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.server.ResponseStatusException;
+import org.springframework.http.HttpStatus;
 
 @RestController
 @RequestMapping("/api/users")
 
 public class UserController {
     private final UserRepository userRepository;
-    public UserController(UserRepository userRepository){
-        this.userRepository =userRepository;
+
+    public UserController(UserRepository userRepository) {
+        this.userRepository = userRepository;
     }
+
     @PostMapping
     public User create(@RequestBody User user) {
-        
+
         return userRepository.save(user);
     }
+
     @GetMapping
-    public List<User> findAll(){
+    public List<User> findAll() {
         return userRepository.findAll();
     }
+
+    // READ BY ID
+    @GetMapping("/{id}")
+    public User findByID(@PathVariable Long id) {
+        return userRepository.findById(id)
+                .orElseThrow(() -> new ResponseStatusException(
+                        HttpStatus.NOT_FOUND,
+                        "Usuario no encontrado"));
+    }
+
+    @PutMapping("/{id}")
+    public User updateUser(@PathVariable Long id, @RequestBody User userDetails) {
+        User user = userRepository.findById(id).orElseThrow(() -> new RuntimeException("Usuario no encontrado"));
+        user.setUsername(userDetails.getUsername());
+        user.setEmail(userDetails.getEmail());
+        return userRepository.save(user);
+    }
+
     
+
 }
